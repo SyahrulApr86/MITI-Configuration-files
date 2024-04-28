6c2675facddc21ef5cd32b7a473f9c8bbb4a6601
SyahrulApr86 <94279620+SyahrulApr86@users.noreply.github.com>
2024-04-28 22:35:18 +0700

    create joomla ecommerce

--- MySQL/init.sql
+++ MySQL/init.sql
@@ -1,7 +1,9 @@
 CREATE DATABASE IF NOT EXISTS wordpressweb;
 CREATE DATABASE IF NOT EXISTS wordpressecomm;
 CREATE DATABASE IF NOT EXISTS nextclouddb;
+CREATE DATABASE IF NOT EXISTS joomla;
 GRANT ALL PRIVILEGES ON wordpressweb.* TO 'wordpressuser'@'%';
 GRANT ALL PRIVILEGES ON wordpressecomm.* TO 'wordpressuser'@'%';
 GRANT ALL PRIVILEGES ON nextclouddb.* TO 'wordpressuser'@'%';
+GRANT ALL PRIVILEGES ON joomla.* TO 'wordpressuser'@'%';
 FLUSH PRIVILEGES;
